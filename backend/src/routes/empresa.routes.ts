554a919707c26794bca56db997c72a0f37684d50
Xaolin997 <xaolin997@gmail.com>
2026-07-14 17:13:50 -0300

    rotas da empresa adicionado

--- backend/src/routes/empresa.routes.ts
+++ backend/src/routes/empresa.routes.ts
@@ -1,13 +1,25 @@
 import type { FastifyInstance } from "fastify";
+
 import {
+  atualizarEmpresaController,
+  buscarEmpresaController,
   criarEmpresaController,
-  listarEmpresasController
+  excluirEmpresaController,
+  listarEmpresasController,
 } from "../controllers/empresa.controller.js";
+
 import { verificarAutenticacao } from "../middlewares/autenticacao.js";
 
 export async function empresaRoutes(app: FastifyInstance) {
   app.addHook("preHandler", verificarAutenticacao);
 
   app.post("/", criarEmpresaController);
+
   app.get("/", listarEmpresasController);
+
+  app.get("/:id", buscarEmpresaController);
+
+  app.patch("/:id", atualizarEmpresaController);
+
+  app.delete("/:id", excluirEmpresaController);
 }
